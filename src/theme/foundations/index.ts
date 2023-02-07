24f7cf490acb2116a6e0786a81eac9159ca57c18
Yoann Fleury <yoann.fleury@yahoo.com>
2023-02-07 13:01:03 +0100

    feat: add inter typography to theme

--- src/theme/foundations/index.ts
+++ src/theme/foundations/index.ts
@@ -1,11 +1,13 @@
 import { colors } from "./colors";
 import { shadows } from "./shadows";
 import { spacing } from "./spacing";
+import { typography } from "./typography";
 
 const foundations = {
   colors,
   shadows,
   space: spacing,
+  ...typography,
 };
 
 export default foundations;
