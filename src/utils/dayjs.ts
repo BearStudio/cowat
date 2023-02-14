38c6b370f26cbefb86442c10c73d8379a0b825b4
Ivan Dalmet <ivan@dalmet.fr>
2023-02-14 12:43:17 +0100

    fix: Add eslint rule to prevent dayjs direct import

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -1,7 +1,6 @@
 import dayjsLib from "dayjs";
 
-export const dayjsTz: (
-  ...args: Parameters<typeof dayjsLib>
-) => ReturnType<typeof dayjsLib.tz> = (...args) => dayjsLib(...args).tz();
+export const dayjsTz = (...args: Parameters<typeof dayjsLib>) =>
+  dayjsLib(...args).tz();
 
 export const dayjsTzConfig = dayjsLib.tz;
