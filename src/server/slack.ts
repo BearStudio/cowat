f3761b66df75df70564f6c3261fed7b2f8756e17
Yoann Fleury <yoann.fleury@yahoo.com>
2023-02-23 12:36:47 +0100

    fix(#47): notify driver when passenger cancel a booking
    closes #47

--- src/server/slack.ts
+++ src/server/slack.ts
@@ -5,6 +5,29 @@ import dayjs from "dayjs";
 import { FULL_TEXT_DATE_WITH_TIME } from "@/constants/dates";
 import { App, LogLevel } from "@slack/bolt";
 
+type PassengerOnStopNotification = Prisma.PassengersOnStopsGetPayload<{
+  include: {
+    stop: {
+      include: {
+        commute: {
+          include: {
+            createdBy: {
+              include: {
+                accounts: true;
+              };
+            };
+          };
+        };
+      };
+    };
+    user: {
+      include: {
+        accounts: true;
+      };
+    };
+  };
+}>;
+
 export const notify = SlackNotify(env.SLACK_WEBHOOK_URL);
 
 const app = new App({
@@ -47,17 +70,6 @@ const newCommute = async (
         stop.time ? ` - ${stop.time}` : ""
       }*\n${stop.location?.address}`,
     },
-    // accessory: {
-    //   type: "button",
-    //   text: {
-    //     type: "plain_text",
-    //     emoji: true,
-    //     text: "Maps",
-    //   },
-    //   value: "value",
-    //   url: searchOnMaps(stop.location?.address ?? ""),
-    //   action_id: "button-action",
-    // },
   }));
 
   return notify.send({
@@ -74,17 +86,6 @@ const newCommute = async (
             commute.seats
           } seats available)`,
         },
-        // accessory: {
-        //   type: "button",
-        //   text: {
-        //     type: "plain_text",
-        //     text: "Check",
-        //     emoji: true,
-        //   },
-        //   value: "click_me_123",
-        //   url: "http://localhost:3000/commutes",
-        //   action_id: "button-action",
-        // },
       },
       {
         type: "divider",
@@ -112,30 +113,7 @@ const newCommute = async (
   } as any);
 };
 
-const newBookingFrom = async (
-  passengerOnStop: Prisma.PassengersOnStopsGetPayload<{
-    include: {
-      stop: {
-        include: {
-          commute: {
-            include: {
-              createdBy: {
-                include: {
-                  accounts: true;
-                };
-              };
-            };
-          };
-        };
-      };
-      user: {
-        include: {
-          accounts: true;
-        };
-      };
-    };
-  }>
-) => {
+const newBookingFrom = async (passengerOnStop: PassengerOnStopNotification) => {
   const driverSlackId = passengerOnStop.stop?.commute?.createdBy?.accounts.find(
     (account) => account.provider === "slack"
   )?.providerAccountId;
@@ -159,9 +137,9 @@ const newBookingFrom = async (
             type: "mrkdwn",
             text: `Hey, 🙋 ${passenger} requested a seat on your *${
               passengerOnStop.stop?.commute?.date
-                ? dayjs(passengerOnStop?.stop?.commute?.date).format(
-                    "dddd DD MMM HH:mm"
-                  )
+                ? dayjs(passengerOnStop?.stop?.commute?.date)
+                    .tz("Europe/Paris")
+                    .format("dddd DD MMM HH:mm")
                 : ""
             }* commute.`,
           },
@@ -177,30 +155,7 @@ const newBookingFrom = async (
   }
 };
 
-const request = async (
-  passengerOnStop: Prisma.PassengersOnStopsGetPayload<{
-    include: {
-      stop: {
-        include: {
-          commute: {
-            include: {
-              createdBy: {
-                include: {
-                  accounts: true;
-                };
-              };
-            };
-          };
-        };
-      };
-      user: {
-        include: {
-          accounts: true;
-        };
-      };
-    };
-  }>
-) => {
+const request = async (passengerOnStop: PassengerOnStopNotification) => {
   const driverSlackId = passengerOnStop.stop.commute?.createdBy?.accounts.find(
     (account) => account.provider === "slack"
   )?.providerAccountId;
@@ -230,9 +185,48 @@ const request = async (
       passengerOnStop.requestStatus === "ACCEPTED" ? "✅" : "❌"
     } ${driver} ${passengerOnStop.requestStatus.toLocaleLowerCase()} your request for *${
               passengerOnStop.stop.commute?.date
-                ? dayjs(passengerOnStop.stop.commute.date).format(
-                    "dddd DD MMM HH:mm"
-                  )
+                ? dayjs(passengerOnStop.stop.commute.date)
+                    .tz("Europe/Paris")
+                    .format("dddd DD MMM HH:mm")
+                : ""
+            }* commute.`,
+          },
+        },
+      ],
+    });
+
+    // TODO: handle result not OK with some logsnag or else
+    console.log(result);
+  } catch (error) {
+    // TODO: handle with some tool to store errors
+    console.error(error);
+  }
+};
+
+const bookingCanceled = async (
+  passengerOnStop: PassengerOnStopNotification
+) => {
+  const driverSlackId = passengerOnStop.stop.commute?.createdBy?.accounts.find(
+    (account) => account.provider === "slack"
+  )?.providerAccountId;
+
+  const passengerSlackId = passengerOnStop.user.accounts.find(
+    (account) => account.provider === "slack"
+  )?.providerAccountId;
+
+  try {
+    const result = await app.client.chat.postMessage({
+      channel: driverSlackId ?? "",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `🙅 ${passengerSlackId} cancelled their booking for the *${
+              passengerOnStop.stop.commute?.date
+                ? dayjs(passengerOnStop.stop.commute.date)
+                    .tz("Europe/Paris")
+                    .format("dddd DD MMM HH:mm")
                 : ""
             }* commute.`,
           },
@@ -252,4 +246,5 @@ export const slack = {
   newBookingFrom,
   newCommute,
   request,
+  bookingCanceled,
 };
