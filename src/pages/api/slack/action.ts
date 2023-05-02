f9ba0cf87ee0367ac66d6e13fb6db731eded1370
Yoann Fleury <yoann.fleury@yahoo.com>
2023-05-02 20:37:39 +0200

    feat: add catch to make sure the user exist in db with slack id

--- src/pages/api/slack/action.ts
+++ src/pages/api/slack/action.ts
@@ -3,48 +3,57 @@ import type { BlockAction, ButtonAction } from "@slack/bolt";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/server/db";
 import { TRPCError } from "@trpc/server";
-import { slackApp } from "@/server/slack";
 
 type SlackPayload = BlockAction<ButtonAction>;
 
 async function bookHandler(action: ButtonAction, slackPayload: SlackPayload) {
-  const user = await prisma.account.findFirstOrThrow({
-    where: {
-      providerAccountId: slackPayload.user.id,
-    },
-    select: { userId: true },
-  });
-
   try {
-    await book({
-      userId: user?.userId,
-      prisma,
-      input: {
-        stopId: action.value,
+    const user = await prisma.account.findFirstOrThrow({
+      where: {
+        providerAccountId: slackPayload.user.id,
       },
+      select: { userId: true },
+    });
+
+    try {
+      await book({
+        userId: user?.userId,
+        prisma,
+        input: {
+          stopId: action.value,
+        },
+      });
+    } catch (error) {
+      if (error instanceof TRPCError) {
+        await fetch(slackPayload.response_url, {
+          method: "POST",
+          body: JSON.stringify({
+            text: `❌ ${error.message}`,
+            replace_original: false,
+          }),
+        });
+
+        return;
+      }
+    }
+
+    // TODO check if bolt can be used
+    await fetch(slackPayload.response_url, {
+      method: "POST",
+      body: JSON.stringify({
+        text: `✅ <@${slackPayload.user.id}> Your booking request has been sent to the driver`,
+        replace_original: false,
+      }),
     });
   } catch (error) {
-    if (error instanceof TRPCError) {
-      await fetch(slackPayload.response_url, {
-        method: "POST",
-        body: JSON.stringify({
-          text: `❌ ${error.message}`,
-          replace_original: false,
-        }),
-      });
-
-      return;
-    }
+    await fetch(slackPayload.response_url, {
+      method: "POST",
+      body: JSON.stringify({
+        text: `❌ It looks like you did not sign up using your Slack account on Cowat, please do to book from Slack`,
+        replace_original: false,
+      }),
+    });
   }
-
-  // TODO check if bolt can be used
-  await fetch(slackPayload.response_url, {
-    method: "POST",
-    body: JSON.stringify({
-      text: `✅ <@${slackPayload.user.id}> Your booking request has been sent to the driver`,
-      replace_original: false,
-    }),
-  });
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
