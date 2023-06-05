e5ef157e9ae96b32a0395577e6761150150d82e8
Yoann Fleury <yoann.fleury@yahoo.com>
2023-06-05 18:39:37 +0200

    feat(admin): filter on deleted commute for stats

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -2,11 +2,23 @@ import { adminProcedure, createTRPCRouter } from "@/server/api/trpc";
 
 export const adminRouter = createTRPCRouter({
   getStats: adminProcedure.query(async ({ ctx }) => {
-    const numberOfCommutesTotal = await ctx.prisma.commute.count();
+    const numberOfCommutesTotal = await ctx.prisma.commute.count({
+      where: {
+        isDeleted: false,
+      },
+    });
     const numberOfCommutePerUsers = await ctx.prisma.user.findMany({
       include: {
         _count: {
-          select: { commutes: true },
+          select: {
+            commutes: {
+              where: {
+                isDeleted: {
+                  equals: false,
+                },
+              },
+            },
+          },
         },
       },
     });
