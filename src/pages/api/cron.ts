12b526119b27be83178c581e9f7d1e144550655c
Yoann Fleury <yoann.fleury@yahoo.com>
2023-07-04 13:07:37 +0200

    feat(#70): auto post commutes of the day

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,120 @@
+import { notify } from "@/server/slack";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/server/db";
+import dayjs from "dayjs";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const commutes = await prisma?.commute.findMany({
+    where: {
+      date: {
+        gte: dayjs().startOf("day").toDate(),
+        lte: dayjs().endOf("day").toDate(),
+      },
+    },
+    include: {
+      createdBy: {
+        select: { id: true, name: true, email: true, image: true },
+      },
+      stops: {
+        include: {
+          location: {
+            select: {
+              address: true,
+              name: true,
+            },
+          },
+          passengers: {
+            include: {
+              user: {
+                select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  image: true,
+                },
+              },
+            },
+            where: {
+              requestStatus: {
+                not: "REFUSED",
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const messageOfTheDay = {
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Today's commutes",
+          emoji: true,
+        },
+      },
+      {
+        type: "divider",
+      },
+      ...commutes
+        .map((commute) => [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*${
+                commute.createdBy?.name ?? commute.createdBy?.email
+              }'s commute*\n${dayjs(commute.date).format(
+                "dddd, MMMM DD hh:mm A"
+              )}\n${commute.stops.length} stop(s)\n${
+                commute.stops.flatMap((stop) => stop.passengers).length
+              } passenger(s)`,
+            },
+            accessory: {
+              type: "image",
+              image_url: commute.createdBy?.image,
+              alt_text: commute.createdBy?.name ?? commute.createdBy?.email,
+            },
+          },
+          ...commute.stops.flatMap((stop) => [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `:round_pushpin: *${stop.location?.address}*\n${stop.location?.name}.`,
+              },
+            },
+            ...stop.passengers.map((passenger) => ({
+              type: "context",
+              elements: [
+                {
+                  type: "image",
+                  image_url: passenger.user.image,
+                  alt_text: passenger.user.name ?? passenger.user.email,
+                },
+                {
+                  type: "plain_text",
+                  emoji: true,
+                  text: passenger.user.name ?? passenger.user.email,
+                },
+              ],
+            })),
+          ]),
+          { type: "divider" },
+        ])
+        .flat(),
+    ],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } as any;
+
+  await notify.send(messageOfTheDay);
+
+  response.status(200).json({
+    ok: true,
+  });
+}
