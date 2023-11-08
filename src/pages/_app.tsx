e7cd959a8999af33f2a95369830ea356a43d3ca9
Yoann Fleury <yoann.fleury@yahoo.com>
2023-11-08 16:42:43 +0100

    fix(#64): add bottom env space
    closes #64

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { api } from "../utils/api";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useTimezone } from "@/components/Timezone";
 import { EnvDevHint } from "@/components/EnvDevHint";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -17,13 +18,21 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   useTimezone();
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
-        <EnvDevHint />
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ChakraProvider>
-    </SessionProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,viewport-fit=cover"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <ChakraProvider theme={theme}>
+          <EnvDevHint />
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ChakraProvider>
+      </SessionProvider>
+    </>
   );
 };
 
