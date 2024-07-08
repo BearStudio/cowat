2bd62076d5c94455d8146d1faee9b1d13b7a5085
Goujon Mathis <mathis.goujon27@gmail.com>
2024-07-08 14:45:23 +0200

    feat(#76): First draft for the auto-accept feature

---
+++ src/components/FieldSwitch/index.tsx
@@ -0,0 +1,63 @@
+import type { FormGroupProps } from "@/components/FormGroup";
+import { FormGroup } from "@/components/FormGroup";
+import type { FieldProps } from "@formiz/core";
+
+import React, { useEffect, useState } from "react";
+import { useField } from "@formiz/core";
+import type { SwitchProps } from "@chakra-ui/react";
+import { Switch } from "@chakra-ui/react";
+
+export type FieldSwitchProps = FieldProps<boolean> &
+  FormGroupProps &
+  Omit<SwitchProps, "children"> & {
+    autoFocus?: boolean;
+  };
+
+export const FieldSwitch = (props: FieldSwitchProps) => {
+  const {
+    errorMessage,
+    id,
+    isValid,
+    isPristine,
+    isSubmitted,
+    isValidating,
+    resetKey,
+    setValue,
+    value,
+    otherProps,
+  } = useField(props);
+
+  const { label, helper, size = "md", autoFocus, ...rest } = otherProps;
+
+  const { required } = props;
+  const [isTouched, setIsTouched] = useState(false);
+  const showError = !isValid && ((isTouched && !isPristine) || isSubmitted);
+
+  useEffect(() => {
+    setIsTouched(false);
+  }, [resetKey]);
+
+  const formGroupProps = {
+    errorMessage,
+    helper,
+    id,
+    isRequired: !!required,
+    label,
+    showError,
+    ...rest,
+  };
+
+  return (
+    <FormGroup {...formGroupProps}>
+      <Switch
+        isChecked={Boolean(value)}
+        isDisabled={isValidating}
+        size={size}
+        autoFocus={autoFocus}
+        // Type error here, require help with formiz
+        onChange={(e) => setValue(e.target.checked)}
+        {...rest}
+      />
+    </FormGroup>
+  );
+};
