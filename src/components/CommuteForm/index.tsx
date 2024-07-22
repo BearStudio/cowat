d495363c4e36725a0a4433c255ae4a2e9d3f7f67
Marie Douet <mdouet@MacBook-Pro-de-Marie.local>
2024-07-22 17:55:17 +0200

    Fix(#144) : Fix comments

--- src/components/CommuteForm/index.tsx
+++ src/components/CommuteForm/index.tsx
@@ -130,7 +130,7 @@ export const CommuteForm = ({
       )}
       <>
         {stops.keys.map((key, index) => {
-          const stopIndex = !Number.isNaN(parseInt(key)) // index is not update when you delete stops above in the list, but key is
+          const stopIndex = Number.isInteger(parseInt(key)) // index is not update when you delete stops above in the list, but key is
             ? parseInt(key)
             : index;
           const stop = commute.data?.stops[stopIndex];
