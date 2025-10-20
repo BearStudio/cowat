3a6cb112cd743fda138efa0eb354244c06bef38b
Nikola LEGAY <nikola.legay@bearstudio.fr>
2025-10-20 14:27:12 +0200

    fix : add comment

--- prisma/migrations/20251008145950_commute_type_feature/migration.sql
+++ prisma/migrations/20251008145950_commute_type_feature/migration.sql
@@ -1,7 +1,9 @@
 -- CreateEnum
+-- TripType : Passenger can book a commute for round trip, oneway trip or return trip
 CREATE TYPE "TripType" AS ENUM ('ROUND', 'ONEWAY', 'RETURN');
 
 -- CreateEnum
+-- CommuteType : We can create a round commute or oneway commute
 CREATE TYPE "CommuteType" AS ENUM ('ROUND', 'ONEWAY');
 
 -- AlterTable
