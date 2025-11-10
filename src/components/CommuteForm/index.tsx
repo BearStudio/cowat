19db4a953485c9313485637f9df995825bb3e3ae
Nikola LEGAY <nikola.legay@bearstudio.fr>
2025-11-10 09:24:41 +0100

    fix (#192) : rename isEditable to isDisabled

--- src/components/CommuteForm/index.tsx
+++ src/components/CommuteForm/index.tsx
@@ -167,7 +167,7 @@ export const CommuteForm = ({
         <LocationField
           name="outwardLocation"
           label="📍 Outward location"
-          isEditable={!arePassengersOnStopsOutward}
+          isDisabled={arePassengersOnStopsOutward}
         />
         <FieldTime
           label="🕑 Outward time"
@@ -257,7 +257,7 @@ export const CommuteForm = ({
           <LocationField
             name="inwardLocation"
             label="📍 Inward location"
-            isEditable={!arePassengersOnStopsInward}
+            isDisabled={arePassengersOnStopsInward}
           />
           <FieldTime
             label="🕑 Inward time"
