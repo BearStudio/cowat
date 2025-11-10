0b8d6e831b81982c787e1e7ecbd8c9dac91ddc67
Nikola LEGAY <nikola.legay@bearstudio.fr>
2025-11-10 09:52:48 +0100

    fix (#192) : split commuteForm file

--- src/components/CommuteForm/index.tsx
+++ src/components/CommuteForm/index.tsx
@@ -1,15 +1,11 @@
 import { AddPlaceholder } from "@/components/AddPlaceholder";
 import { FieldDayPicker } from "@/components/FieldDatePicker";
-import { FieldSelect } from "@/components/FieldSelect";
 import { FieldTextarea } from "@/components/FieldTextarea";
 import { FieldTime } from "@/components/FieldTime";
-import { FieldHidden } from "@/components/FieldHidden";
 import { FieldNumber } from "@/components/FieldNumber";
 import { FieldInput } from "@/components/FieldInput";
 import { FieldCheckbox } from "@/components/FieldCheckbox";
 import { Icon } from "@/components/Icon";
-import { LocationForm } from "@/components/LocationForm";
-import type { RouterInputs } from "@/utils/api";
 import { api } from "@/utils/api";
 import { getAllPassengers } from "@/utils/commutes";
 import {
@@ -18,34 +14,17 @@ import {
   AlertIcon,
   AlertTitle,
   Box,
-  Button,
   Divider,
   Flex,
-  HStack,
-  IconButton,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Stack,
-  useDisclosure,
 } from "@chakra-ui/react";
-import {
-  Formiz,
-  useCollection,
-  useForm,
-  useFormContext,
-  useFormFields,
-} from "@formiz/core";
+import { useCollection, useFormContext, useFormFields } from "@formiz/core";
 import { isMaxNumber, isMinNumber } from "@formiz/validations";
-import { Plus, Trash } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useRouter } from "next/router";
 import { Fragment, useEffect } from "react";
 import dayjs from "dayjs";
 import { LocationField } from "@/components/LocationField";
+import { Stop } from "@/components/Stop";
 
 type CommuteFormProps = {
   repeaterInitialValues: Array<object>;
@@ -275,141 +254,6 @@ export const CommuteForm = ({
         </Flex>
       )}
       <FieldTextarea label="Comment" name="comment" />
-    </>
-  );
-};
-
-type StopProps = {
-  id?: string;
-  index: number;
-  isRemovable?: boolean;
-  isEditable?: boolean;
-  onRemove: () => void;
-};
-
-const Stop = ({
-  id,
-  index,
-  onRemove,
-  isEditable = true,
-  isRemovable = true,
-}: StopProps) => {
-  const ctx = api.useContext();
-  const form = useFormContext();
-  const formFields = useFormFields({
-    fields: ["stops", "outwardLocation", "inwardLocation"] as const,
-    selector: "value",
-  });
-
-  const newLocationForm = useForm({
-    onValidSubmit: (values: RouterInputs["location"]["create"]) => {
-      createLocation.mutate(values);
-    },
-  });
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const locations = api.location.mine.useQuery();
-
-  // Locations already used in a stop
-  const usedLocations = [
-    ...(formFields.stops?.map((stop: { location: string }) => stop.location) ??
-      []),
-    formFields.outwardLocation,
-    formFields.inwardLocation,
-  ].filter(Boolean);
-
-  const getSelectOptions = (index: number) => {
-    // we keep as options the unused locations & the value of current stop location
-    return (
-      locations?.data?.filter(
-        (location) =>
-          !usedLocations?.includes(location.id) ||
-          location.id === formFields.stops?.[index]?.location
-      ) ?? []
-    );
-  };
-
-  const createLocation = api.location.create.useMutation({
-    onSuccess: async ({ id }) => {
-      await ctx.location.invalidate();
-      form.setValues({
-        [`stops[${index}].location`]: id,
-      });
-      onClose();
-    },
-  });
-
-  return (
-    <>
-      <Stack
-        align="flex-start"
-        direction={{ base: "column", md: "row" }}
-        spacing={{ base: 2, md: 6 }}
-      >
-        <HStack align="flex-start" w="full">
-          <FieldHidden name={`stops[${index}].id`} defaultValue={id} />
-          <FieldSelect
-            label={`📍 Stop ${index + 1}`}
-            name={`stops[${index}].location`}
-            isDisabled={!isEditable}
-            placeholder="Please select a location"
-            options={
-              getSelectOptions(index)?.map((location) => ({
-                label: location.name,
-                value: location.id,
-              })) ?? []
-            }
-          />
-          <Box pt={8}>
-            <IconButton
-              aria-label="Add a location"
-              icon={<Icon icon={Plus} />}
-              isDisabled={!isEditable}
-              onClick={onOpen}
-            />
-          </Box>
-        </HStack>
-        <HStack align="flex-start" spacing={{ base: 2, md: 6 }} w="full">
-          <FieldTime
-            label="🕑 Pick up time"
-            name={`stops[${index}].time`}
-            isDisabled={!isEditable}
-          />
-          <Box pt={8}>
-            <IconButton
-              variant="danger"
-              aria-label={`Remove stop ${index}`}
-              icon={<Icon icon={Trash} />}
-              onClick={() => onRemove()}
-            />
-          </Box>
-        </HStack>
-      </Stack>
-      {isOpen && (
-        <Modal isOpen onClose={onClose} size="sm">
-          <ModalOverlay />
-          <Formiz connect={newLocationForm}>
-            <ModalContent>
-              <ModalHeader flex="1">New location</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <LocationForm />
-              </ModalBody>
-
-              <ModalFooter>
-                <Button
-                  variant="primary"
-                  isLoading={createLocation.isLoading}
-                  onClick={() => newLocationForm.submit()}
-                >
-                  Save
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Formiz>
-        </Modal>
-      )}
     </>
   );
 };
