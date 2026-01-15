bfac414ef86c019b1de6da97dccdbf507ee2c5ea
Nikola LEGAY <nikola.legay@bearstudio.fr>
2026-01-15 16:39:56 +0100

    feat (#191 : add getByYearsWithDate +  getByYears

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -3,16 +3,41 @@ import dayjs from "dayjs";
 
 type PassengerWithCommuteDateType = PassengersOnStops & { commuteDate?: Date };
 
+const currentYear = dayjs().year();
+const years = [currentYear - 3, currentYear - 2, currentYear - 1, currentYear];
+
 export const getAll = (items: Commute[] | PassengersOnStops[]) => {
   return items.length;
 };
 
-export const getYear = (items: Commute[]) => {
+export const getActualYear = (items: Commute[]) => {
   return items.filter((item) => dayjs(item.createdAt).isSame(dayjs(), "year"))
     .length;
 };
 
-export const getYearWithDate = (items: PassengerWithCommuteDateType[]) => {
+export const getActualYearWithDate = (
+  items: PassengerWithCommuteDateType[]
+) => {
   return items.filter((item) => dayjs(item.commuteDate).isSame(dayjs(), "year"))
     .length;
+};
+
+export const getByYears = (items: Commute[]) => {
+  return years.map((year) => ({
+    year,
+    value: items.filter((item) =>
+      dayjs(item.createdAt).isSame(dayjs().year(year), "year")
+    ).length,
+    actualYear: year === currentYear,
+  }));
+};
+
+export const getByYearsWithDate = (items: PassengerWithCommuteDateType[]) => {
+  return years.map((year) => ({
+    year,
+    value: items.filter((item) =>
+      dayjs(item.commuteDate).isSame(dayjs().year(year), "year")
+    ).length,
+    actualYear: year === currentYear,
+  }));
 };
