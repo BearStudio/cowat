00148f317efeba345df96f72732b21b5b5fc07ce
Nikola LEGAY <nikola.legay@bearstudio.fr>
2026-01-20 15:10:10 +0100

    feat (#191) : update stat page

--- src/pages/account/statistics/index.tsx
+++ src/pages/account/statistics/index.tsx
@@ -1,7 +1,13 @@
 import { Icon } from "@/components/Icon";
 import { LayoutAuthenticated } from "@/layout/LayoutAuthenticated";
 import { api } from "@/utils/api";
-import { getAll, getYear, getYearWithDate } from "@/utils/stats";
+import {
+  getAll,
+  getActualYear,
+  getActualYearWithDate,
+  getByYears,
+  getByYearsWithDate,
+} from "@/utils/stats";
 import {
   Center,
   Flex,
@@ -9,12 +15,11 @@ import {
   HStack,
   IconButton,
   Spinner,
-  Stack,
-  Text,
 } from "@chakra-ui/react";
 import { ArrowLeft } from "lucide-react";
 import Head from "next/head";
 import Link from "next/link";
+import { ChatWithStats } from "@/components/ChatWithStats";
 
 const StatsPage = () => {
   const profile = api.user.profile.useQuery();
@@ -46,47 +51,29 @@ const StatsPage = () => {
           <Spinner />
         </Center>
       )}
-      <Stack
-        spacing="4"
-        as="main"
-        boxShadow="card"
-        p="4"
-        bg="white"
-        rounded="md"
-        _dark={{
-          bg: "gray.800",
-        }}
-      >
-        <Flex flex={1} direction={"column"} gap={6}>
-          <Stack>
-            <Text fontWeight="bold">All</Text>
-            <Text>
-              Commutes : {commuteStats.data && getAll(commuteStats.data)}
-            </Text>
-            <Text>
-              Booked commutes : {bookedStats.data && getAll(bookedStats.data)}
-            </Text>
-            <Text>
-              Driven people :{" "}
-              {drivenPeopleStats.data && getAll(drivenPeopleStats.data)}
-            </Text>
-          </Stack>
-          <Stack>
-            <Text fontWeight="bold">This year</Text>
-            <Text>
-              Commutes : {commuteStats.data && getYear(commuteStats.data)}
-            </Text>
-            <Text>
-              Booked commutes : {bookedStats.data && getYear(bookedStats.data)}
-            </Text>
-            <Text>
-              Driven people:{" "}
-              {drivenPeopleStats.data &&
-                getYearWithDate(drivenPeopleStats.data)}
-            </Text>
-          </Stack>
-        </Flex>
-      </Stack>
+      <Flex direction="column" gap="6" as="main">
+        <ChatWithStats
+          label="Commutes"
+          chartData={getByYears(commuteStats.data || [])}
+          allTimeData={commuteStats.data && getAll(commuteStats.data)}
+          thisYearData={commuteStats.data && getActualYear(commuteStats.data)}
+        />
+        <ChatWithStats
+          label="Booked commutes"
+          chartData={getByYears(bookedStats.data || [])}
+          allTimeData={bookedStats.data && getAll(bookedStats.data)}
+          thisYearData={bookedStats.data && getActualYear(bookedStats.data)}
+        />
+        <ChatWithStats
+          label="Driven people"
+          chartData={getByYearsWithDate(drivenPeopleStats.data || [])}
+          allTimeData={drivenPeopleStats.data && getAll(drivenPeopleStats.data)}
+          thisYearData={
+            drivenPeopleStats.data &&
+            getActualYearWithDate(drivenPeopleStats.data)
+          }
+        />
+      </Flex>
     </LayoutAuthenticated>
   );
 };
