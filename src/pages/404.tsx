ae15de5d8e8dd4c364df4468aa1723da7eb2d84e
Marie <marie.douet421@gmail.com>
2026-02-13 17:23:12 +0100

    Fix v2

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ import { Page, PageBottomBar, PageContent } from "@/components/Page";
 const Page404 = () => {
   return (
     <Page>
-      <p>You've reached an error.</p>
+      <p>You ve reached an error.</p>
       <PageContent>
         <ErrorPage errorCode={404} />
       </PageContent>
